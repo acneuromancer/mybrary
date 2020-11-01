037d5ec7fc7f14572a4cf44ba0efcbb4ed5676d5
acneuromancer <dokukodo108@gmail.com>
2020-11-01 18:04:40 +0100

    Finished Author routes

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const bodyParser = require('body-parser');
-
+const methodOverride = require('method-override');
 const indexRouter = require('./routes/index');
 const authorRouter = require('./routes/authors');
 const bookRouter = require('./routes/books');
@@ -20,6 +20,7 @@ app.use(bodyParser.urlencoded({
   limit: '10mb',
   extended: false
 }));
+app.use(methodOverride('_method'));
 
 const mongoose = require('mongoose');
 
